a100042940b1750a3c5dc6538744b6eb6528c9ef
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-02-14 12:41:12 -0600

    Back to basics to my emacs configuration.

--- init.el
+++ init.el
@@ -1,15 +1,1 @@
-(require 'package)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
-(package-initialize)
-
-(when (not package-archive-contents)
-  (package-refresh-contents))
-
-(defvar my-packages '(starter-kit
-                      starter-kit-lisp)
-  "My base packages")
-
-(dolist (p my-packages)
-  (when (not (package-installed-p p))
-    (package-install p)))
+(message "test")

faeab6d688aa1d257f439657c147451eaa5e7544
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-02-14 17:03:37 -0600

    Added my theme and railscasts theme.

--- init.el
+++ init.el
@@ -1,2 +1,17 @@
 (org-babel-load-file "/Users/hectoregm/.emacs.d/hectoregm.org")
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(py-ipython-command "/usr/local/bin/python")
+ '(py-python-command "/usr/local/bin/python")
+ '(py-python2-command "/usr/local/bin/python")
+ '(py-shell-name "/usr/local/bin/python"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

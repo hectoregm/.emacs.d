9ddae0ca02bd765cbba6ff1dc54df54085db1b8f
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-07-04 23:00:06 -0500

    Remove exec-path configuration.

--- init.el
+++ init.el
@@ -5,9 +5,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(exec-path
-   (quote
-    ("/usr/bin" "/bin" "/usr/sbin" "/sbin" "/opt/homebrew-cask/Caskroom/emacs/24.4/Emacs.app/Contents/MacOS/bin-x86_64-10_9" "/opt/homebrew-cask/Caskroom/emacs/24.4/Emacs.app/Contents/MacOS/libexec-x86_64-10_9" "/opt/homebrew-cask/Caskroom/emacs/24.4/Emacs.app/Contents/MacOS/libexec" "/opt/homebrew-cask/Caskroom/emacs/24.4/Emacs.app/Contents/MacOS/bin" "/usr/local/bin")))
  '(py-ipython-command "/usr/local/bin/python")
  '(py-python-command "/usr/local/bin/python")
  '(py-python2-command "/usr/local/bin/python")
